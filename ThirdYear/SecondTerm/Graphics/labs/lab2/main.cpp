90425414668884068f7303a2ac25983a03821c15
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-02-27 15:36:22 +0200

    adding the glfw library to be able to deal withgpu

--- ThirdYear/SecondTerm/Graphics/labs/lab2/main.cpp
+++ ThirdYear/SecondTerm/Graphics/labs/lab2/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+/**
+ * @brief to be able to deal with GPU we will deal with a library called glfw
+ *
+ * @return int
+ */
+#include <GLFW/glfw3.h>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main(int, char **)
+{
+    std::cout << "GOO, world!\n";
 }
